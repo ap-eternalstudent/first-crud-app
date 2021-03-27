ae5122ba2523cac4bcecc94e106ca9383c52d5d8
anthony28 <anthony@wearea2.com>
2021-03-26 23:02:57 -0400

    added delete functionality

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const update = document.querySelector("#update-button");
+const deleteButton = document.querySelector("#delete-button");
 
 update.addEventListener("click", (_) => {
   fetch("/quotes", {
@@ -8,5 +9,32 @@ update.addEventListener("click", (_) => {
       name: "Darth Vader",
       quote: "I find your lack of faith disturbing.",
     }),
-  });
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+    })
+    .then((response) => {
+      window.location.reload(true);
+    });
+});
+
+deleteButton.addEventListener("click", (_) => {
+  fetch("/quotes", {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vadar",
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+    })
+    .then((response) => {
+      if (response === "No quote to delete") {
+        messageDiv.textContent = "No Darth Vadar quote to delete";
+      } else {
+        window.location.reload(true);
+      }
+    })
+    .catch(console.error);
 });
--- server.js
+++ server.js
@@ -1,46 +1,80 @@
-const bodyParser = require("body-parser");
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
 
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
+
+// ========================
+// Link to Database
+// ========================
+
 const connectionString =
   "mongodb+srv://anton:753842@cluster0.p6dfi.mongodb.net/star-wars?retryWrites=true&w=majority";
 
+MongoClient.connect(connectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log("Connected to Database");
+    const db = client.db("star-wars-quotes");
+    const quotesCollection = db.collection("quotes");
+    // All your handlers here...
+    // ========================
+    // Middlewares
+    // ========================
+    app.set("view engine", "ejs");
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.static("public"));
+    // ========================
+    // Routes
+    // ========================
+    app.get("/", (req, res) => {
+      db.collection("quotes")
+        .find()
+        .toArray()
+        .then((quotes) => {
+          res.render("index.ejs", { quotes: quotes });
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.put("/quotes", (req, res) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: "Yoda" },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+        .then((result) => res.json("Success"))
+        .catch((error) => console.error(error));
+    });
+    app.delete("/quotes", (req, res) => {
+      quotesCollection
+        .deleteOne({ name: req.body.name })
+        .then((result) => {
+          if (result.deletedCount === 0) {
+            return res.json("No quote to delete");
+          }
+          res.json("Deleted Darth Vadar's quote");
+        })
+        .catch((error) => console.error(error));
+    });
+  }
+);
 app.listen(3000, function () {
   console.log("listening on 3000");
-});
-app.set("view engine", "ejs");
-app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-MongoClient.connect(connectionString, {
-  useUnifiedTopology: true,
-}).then((client) => {
-  console.log("Connected to Database");
-  const db = client.db("star-wars-quotes");
-  const quotesCollection = db.collection("quotes");
-  // All your handlers here...
-
-  app.post("/quotes", (request, response) => {
-    quotesCollection
-      .insertOne(request.body)
-      .then((result) => {
-        response.redirect("/");
-      })
-      .catch((error) => console.error(error));
-  });
-
-  app.get("/", (request, response) => {
-    db.collection("quotes")
-      .find()
-      .toArray()
-      .then((results) => {
-        response.render("index.ejs", { quotes: results });
-      })
-      .catch((error) => console.error(error));
-  });
-  app.put("/quotes", (request, response) => {
-    console.log(request.body);
-  });
 });
